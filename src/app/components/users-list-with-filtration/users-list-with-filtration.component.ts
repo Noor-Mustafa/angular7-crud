ea49c253caaa13e03562d52c12c6b8c0c6c67a32
noormustafa796@gmail.com <noormustafa796@gmail.com>
2019-06-13 03:12:53 +0500

    added new comp with filtration work 'user-list-with-filtration'

--- src/app/components/users-list-with-filtration/users-list-with-filtration.component.ts
+++ src/app/components/users-list-with-filtration/users-list-with-filtration.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+/*
+ * Users Service
+ */
+import { UserService } from '../../services/user-service/user.service';
+
 @Component({
   selector: 'app-users-list-with-filtration',
   templateUrl: './users-list-with-filtration.component.html',
@@ -7,9 +12,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersListWithFiltrationComponent implements OnInit {
 
-  constructor() { }
+
+  private users;
+  private ordered_users;
+
+  private search_key: string;
+  private user_order: string;
+  private loader: boolean;
+
+  constructor(
+    private user_service_instance: UserService
+  ) {
+
+    this.users = this.ordered_users = [];
+    this.user_order = "default";
+
+    this.loader = false;
+  }
 
   ngOnInit() {
+    this.user_service_instance.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
+
+  onScroll() {
+
+    console.log("onScroll()");
+    this.loader = true;
+
+    this.user_service_instance.getUsers().subscribe((data) => {
+      for (let i = 0; i < data.length; i++) {
+        this.users.push(data[i]);
+        // console.log(data[i]); 
+      }
+      this.loader = false;
+      // this.users.push(data[0]);
+      // console.log("data: ", this.users)
+    });
+  }
+
+  private onChangeSearch(): void {
+    this.ordered_users.length = 0;
+    if (this.search_key == "") {
+      return;
+    }
+    else {
+      for (let i = 0; i < this.users.length; i++) {
+        if (this.users[i].login.match(this.search_key)) {
+          this.ordered_users.push(this.users[i]);
+        }
+      }
+    }
+
+  }
+
+  private userOrderChange(order: string): void {
+    this.user_order = order;
+
+    this.ordered_users = [...this.users];
+
+    switch (this.user_order) {
+      case 'default':
+        if (!this.search_key) {
+          this.ordered_users.length = 0;
+        }
+        else {
+          this.onChangeSearch();
+        }
+        // this.ordered_users = [...this.users];
+        break;
+      case 'ascending':
+        if (this.search_key) {
+          this.onChangeSearch();
+        }
+        this.ordered_users = this.ordered_users.sort((a, b) => {
+          if (b.login > a.login) { return -1; }
+          if (b.login < a.login) { return 1; }
+          return 0;
+        });
+        break;
+      case 'descending':
+        if (this.search_key) {
+          this.onChangeSearch();
+        }
+        this.ordered_users = this.ordered_users.sort((a, b) => {
+          if (b.login < a.login) { return -1; }
+          if (b.login > a.login) { return 1; }
+          return 0;
+        });
+        break;
+      default:
+        break;
+    }
+    return;
   }
 
 }
